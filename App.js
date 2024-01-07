882b258919b58da0ac791bfa1e289187ec33251a
Your Name <youremail@yourdomain.com>
2024-01-07 20:28:52 +0300

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hey 123</Text>
+      <Text>Start</Text>
       <StatusBar style="auto" />
     </View>
   );
